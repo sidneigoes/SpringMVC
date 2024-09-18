7b89bba50840b9d79408c27dd459eb78e68cbea4
sidnei <sidneigoes23@gmail.com>
2024-09-17 21:51:03 -0300

    Commit inicial

---
+++ src/main/java/br/com/springmvc/conf/ServletSpringMVC.java
@@ -0,0 +1,20 @@
+package br.com.springmvc.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+// Tem que colocar aqui para ser adicionado no
+//carregamento da servlet base
+        return new Class[]{AppWebConfiguration.class};
+    }
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
